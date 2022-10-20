4d6972ad6a94d0e709bc3bdb2327fdf42c01bdbc
aabhi <aabhishek.keshari@gmail.com>
2022-10-20 11:45:40 +0530

    Address commented

--- crud-hibernate-1/src/main/java/com/crud/hibernate/entity/Address.java
+++ crud-hibernate-1/src/main/java/com/crud/hibernate/entity/Address.java
@@ -1,53 +1,53 @@
-package com.crud.hibernate.entity;
-
-public class Address {
-	
-	private Long houseNo;
-	private String city;
-	private String landmark;
-	private Long pincode;
-	
-	public Address() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public Address(Long houseNo, String city, String landmark, Long pincode) {
-		super();
-		this.houseNo = houseNo;
-		this.city = city;
-		this.landmark = landmark;
-		this.pincode = pincode;
-	}
-	
-	public Long getHouseNo() {
-		return houseNo;
-	}
-	public void setHouseNo(Long houseNo) {
-		this.houseNo = houseNo;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getLandmark() {
-		return landmark;
-	}
-	public void setLandmark(String landmark) {
-		this.landmark = landmark;
-	}
-	public Long getPincode() {
-		return pincode;
-	}
-	public void setPincode(Long pincode) {
-		this.pincode = pincode;
-	}
-	
-	@Override
-	public String toString() {
-		return "Address [houseNo=" + houseNo + ", city=" + city + ", landmark=" + landmark + ", pincode=" + pincode
-				+ "]";
-	}
-}
+//package com.crud.hibernate.entity;
+//
+//public class Address {
+//	
+//	private Long houseNo;
+//	private String city;
+//	private String landmark;
+//	private Long pincode;
+//	
+//	public Address() {
+//		super();
+//		// TODO Auto-generated constructor stub
+//	}
+//	
+//	public Address(Long houseNo, String city, String landmark, Long pincode) {
+//		super();
+//		this.houseNo = houseNo;
+//		this.city = city;
+//		this.landmark = landmark;
+//		this.pincode = pincode;
+//	}
+//	
+//	public Long getHouseNo() {
+//		return houseNo;
+//	}
+//	public void setHouseNo(Long houseNo) {
+//		this.houseNo = houseNo;
+//	}
+//	public String getCity() {
+//		return city;
+//	}
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
+//	public String getLandmark() {
+//		return landmark;
+//	}
+//	public void setLandmark(String landmark) {
+//		this.landmark = landmark;
+//	}
+//	public Long getPincode() {
+//		return pincode;
+//	}
+//	public void setPincode(Long pincode) {
+//		this.pincode = pincode;
+//	}
+//	
+//	@Override
+//	public String toString() {
+//		return "Address [houseNo=" + houseNo + ", city=" + city + ", landmark=" + landmark + ", pincode=" + pincode
+//				+ "]";
+//	}
+//}
